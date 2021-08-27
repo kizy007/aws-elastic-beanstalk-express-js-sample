5988002f488232b6922f1bbe341fc34f7fc402d4
kizy007 <okizzy@gmail.com>
2021-08-27 13:24:17 -0400

    4th message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('what will be will be'));
+app.get('/', (req, res) => res.send('I LOVE LINUX SYSTEM'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
