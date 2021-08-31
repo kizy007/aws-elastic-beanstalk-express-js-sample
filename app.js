3782bf0d0241891d059b385c0c820dbe6f459469
kizy007 <okizzy@gmail.com>
2021-08-31 13:47:45 -0400

    6th message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('test runs'));
+app.get('/', (req, res) => res.send('15 Talking Points ON CICD'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
