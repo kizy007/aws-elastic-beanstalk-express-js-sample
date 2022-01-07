ee75c548fa53a3bc8bc847e0ec930593e4afd6ed
kizito Onoabhagbe <72668178+kizy007@users.noreply.github.com>
2022-01-06 21:37:03 -0500

    updated the note on line 5

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('markers of Christ being billionares 007'));
+app.get('/', (req, res) => res.send('Hello world . I am updating my repository KO));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
